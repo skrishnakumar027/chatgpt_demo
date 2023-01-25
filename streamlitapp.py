05cb552afa2e79586079dfd7664ff1ce6c8ad415
skrishnakumar027 <79400435+skrishnakumar027@users.noreply.github.com>
2023-01-26 00:03:51 +0530

    Add files via upload

---
+++ streamlitapp.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from transformers import pipeline
+from PIL import Image
+
+st.set_page_config(page_title="DTCC ChatPGT Demo", page_icon=":guardsman:", layout="wide")
+st.title("A facial expression and sentiment analyzer")
+
+# Sentiment classifier
+st.subheader("Sentiment Analyzer")
+sentiment_model = pipeline("sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english")
+text = st.text_input("Enter your text:")
+if text:
+    sentiment = sentiment_model(text)[0]["label"]
+    st.write(f"The sentiment of the text is: {sentiment}")
+
+# Facial expression classifier
+st.subheader("Facial Expression Analyzer")
+image_file = st.file_uploader("Upload an image of a face:")
+if image_file:
+    image = Image.open(image_file)
+    facial_expression_model = pipeline("facial-expression", model="Rajaram1996/FacialEmoRecog")
+    facial_expression = facial_expression_model(image)[0]["emotion"]
+    st.image(image, caption=f"The facial expression is: {facial_expression}")
+
